5818ac68c75f68c131ccb41cde1374400955e3d7
federicaarosio <arosio.fede@gmail.com>
2023-12-18 15:00:50 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template lang="">
+  <div>
+    <h1 class="text-danger">
+      Prova
+    </h1>
+  </div>
+</template>
+<script>
+export default {
+  name: 'AppaVue'
+}
+</script>
+<style lang="scss">
+@use './styles/general.scss';
+  
+</style>
